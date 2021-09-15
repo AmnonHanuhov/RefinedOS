c90438a00fa3bd9ee7f85168d98c01a068750b5d
Amnon Hanuhov <AmnonSWE@gmail.com>
2021-09-15 23:46:34 +0300

    Created a minimal Rust kernel
    Signed-off-by: Amnon Hanuhov <AmnonSWE@gmail.com>

--- refined_os/src/main.rs
+++ refined_os/src/main.rs
@@ -12,11 +12,21 @@ fn panic(_info : &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Welcome to RefinedOS!";
 /// Overwriting the entry point as we don't have access
 /// to the Rust runtime and `crt0`.
 /// Disable name mangling to tell the linker the name of the entry point function.
 /// Use `C` calling convention
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 11+(i as u8 % 5);
+        }
+    }
+
     loop {}
 }
